06f14252a99c4b74e60d4f448ea8fa4004fd8761
akarolinada <ana.melo91@outlook.com>
2021-11-28 21:52:33 -0300

    1.0

--- conn.js
+++ conn.js
@@ -1,1 +1,21 @@
+const mongodb = require('mongodb').MongoClient
+const url = 'mongodb+srv://soulcode-g6:14253614@cluster0.4qnzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+const client = new mongodb(url)
+
+//função assincrona - começa a ser executada mas não pára a aplicação, ou seja, continua com a execução das outras rotinas
+async function run(){
+    try{
+        //espera a conexão com o banco, quando essa conexão for estabelecida é impresso no console a mensagem
+        await client.connect()
+        console.log('Estamos conectados ao Banco')
+    }
+    //se não conseguiu a conexao, vai imprimir no console o erro
+    catch(err){
+        console.log(err)
+    }
+}
+//aqui fizemos a chamada da função
+run()
+//aqui fizemos a exportação do módulo
+module.exports = client
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const handle = require('express-handlebars')
+const client = require('./conn')
+const bodyParser = require('body-parser')
+
+const LocalStorage = require('node-localstorage').LocalStorage
+localStorage = new LocalStorage('./scratch')
+
+//para conseguirmos ler uma propriedade do tipo ObjectId precisamos fazer essa importação
+const ObjectId = require('mongodb').ObjectId
+
+const app = express()
+
+const porta = 3000
+
+const dbo = client.db('lojaOnline')
+
+const hbs = handle.create({
+    partialsDir: ('views/partials/')
+})
+
+//dizer para o express que o motor de renderização da página html vai ser o handlebars, através da variável hbs
+app.engine('handlebars', hbs.engine)
+//estamos setando o nosso view enigne com o motor de rederização handle bars
+app.set('view engine', 'handlebars')
+
+//o tráfego de dados pode ser por string
+app.use(express.urlencoded())
+//o tráfego de dados pode ser por objetos json
+app.use(express.json())
+
+//os nosso arquivos estáticos estão na página public
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
